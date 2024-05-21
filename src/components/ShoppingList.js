36508a441983be32bc68e5a7faf4786d3bf88536
SANCHES_VARELA Nicolas <nicolas.sanches_varela@macsf.fr>
2024-05-21 18:08:10 +0200

    prop technique - parent vers enfant

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,6 +1,6 @@
 import '../styles/ShoppingList.css'
 import { plantList } from '../datas/plantList'
-import CareScale from './CareScale'
+import PlantItem from './PlantItem'
 
 function ShoppingList() {
 	const categories = plantList.reduce(
@@ -17,16 +17,14 @@ function ShoppingList() {
 				))}
 			</ul>
 			<ul className='lmj-plant-list'>
-				{plantList.map((plant) => (
-					<li key={ plant.id } className='lmj-plant-item'>
-                        { plant.name } 
-                        { plant.isBestSale && plant.category === 'classique' 
-                        ? <span>🔥</span> 
-                        : null }
-                        {plant.isSpecialOffer && <div className='lmj-sales'>Soldes</div>}
-                        <CareScale careType='water' scaleValue={plant.water} />
-                        <CareScale careType='light' scaleValue={plant.light} />
-                    </li>
+				{plantList.map(({ id, cover, name, water, light }) => (
+					<PlantItem
+                        id={id}
+                        cover={cover}
+                        name={name}
+                        water={water}
+                        light={light}
+                    />
 				))}
 			</ul>
 		</div>
