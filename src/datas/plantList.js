6bdf053019ebd046782c7ec876f5a6abeb968935
SANCHES_VARELA Nicolas <nicolas.sanches_varela@macsf.fr>
2024-05-21 17:20:26 +0200

    liste - conditions react

---
+++ src/datas/plantList.js
@@ -0,0 +1,65 @@
+export const plantList = [
+	{
+		name: 'monstera',
+		category: 'classique',
+        id: '1ed',
+        isBestSale: false,
+        isSpecialOffer: true
+	},
+	{
+		name: 'ficus lyrata',
+		category: 'classique',
+		id: '2ab',
+        isBestSale: false,
+        isSpecialOffer: true
+	},
+	{
+		name: 'pothos argenté',
+		category: 'classique',
+		id: '3sd',
+        isBestSale: false,
+        isSpecialOffer: true
+	},
+	{
+		name: 'yucca',
+		category: 'classique',
+		id: '4kk',
+        isBestSale: true,
+        isSpecialOffer: false
+	},
+	{
+		name: 'olivier',
+		category: 'extérieur',
+		id: '5pl',
+        isBestSale: false,
+        isSpecialOffer: false
+	},
+	{
+		name: 'géranium',
+		category: 'extérieur',
+		id: '6uo',
+        isBestSale: false,
+        isSpecialOffer: false
+	},
+	{
+		name: 'basilique',
+		category: 'extérieur',
+		id: '7ie',
+        isBestSale: true,
+        isSpecialOffer: false
+	},
+	{
+		name: 'aloe',
+		category: 'plante grasse',
+		id: '8fp',
+        isBestSale: false,
+        isSpecialOffer: false
+	},
+	{
+		name: 'succulente',
+		category: 'plante grasse',
+		id: '9vn',
+        isBestSale: true,
+        isSpecialOffer: true
+	}
+]
