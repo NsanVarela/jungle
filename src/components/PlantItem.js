217d8de6790de2a9edca5ea5c7a7b397d741b3b7
SANCHES_VARELA Nicolas <nicolas.sanches_varela@macsf.fr>
2024-05-22 12:09:46 +0200

    ajout d'evenement avec parametre

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -2,8 +2,12 @@ import CareScale from './CareScale'
 import '../styles/PlantItem.css'
 
 function PlantItem({ id, cover, name, water, light }) {
+
+	function handleClick(plantName) {
+		alert(`Vous voulez acheter 1 ${plantName} ? Très bon choix✨`)
+	}
     return (
-        <li key={id} className='lmj-plant-item'>
+        <li key={id} className='lmj-plant-item' onClick={() => handleClick(name)}>
 			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
 			{name}
 			<div>
